92271c2a6ee37df61969fa7a6ffabe22abd936fe
bruic87 <bruic@outlook.com>
2023-09-14 17:23:05 -0300

    'aplicacao de dark mode'

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 export default {
+  darkMode: 'class',
   content: [
     "./index.html",
     "./src/**/*.{js,jsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        dark: {
+          100: 'rgb(18, 18,18)',
+          200: 'rgb(51, 51, 51)'
+        },
+        alura: {
+          100: '#167bf7',
+          200: '#051993'
+        }
+      }
+    }
   },
   plugins: [],
 }
