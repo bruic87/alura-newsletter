823cd59a2d8e60d03bd681e71016da1db773f530
bruic87 <bruic@outlook.com>
2023-09-14 15:35:07 -0300

    'commit inicial curso react + vite + tailwind'

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,15 @@
+const Header = () => {
+  return (
+    <div className="flex h-20 bg-gray-500 justify-between items-center">
+        <span>
+            Olá, Usuário
+        </span>
+        <h1>
+            Alura Newsletter
+        </h1>
+        <input type="checkbox"/>
+    </div>
+  )
+}
+
+export default Header
