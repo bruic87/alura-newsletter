cde2102194ba8978aca29a7c0a0a23b267e261e8
bruic87 <bruic@outlook.com>
2023-09-17 14:36:38 -0300

    'commit final do curso guiado react com tailwind'

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,22 @@
+import { useState } from "react"
+
 import Header from "./components/Header/Header"
+import ArticleList from "./components/ArticleList/ArticleList"
+import Form from "./components/Form/Form"
 
 const App = () => {
+  const [user, setUser] = useState()
+
+  const submitForm = (user) => {
+    setUser(user)
+  }
+
   return (
-    <Header />
+    <div className="w-screen h-screen sm:px-5 flex flex-col justify-start items-center">
+      <Header user={user?.name} />
+      { !!user && <ArticleList /> }
+      { !!user || <Form onSubmit={submitForm} /> }
+    </div>
   )
 }
 
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,15 +1,11 @@
 import ToggleTheme from "../ToggleTheme/ToggleTheme"
 
-const Header = () => {
+const Header = ({ user }) => {
   return (
-    <div className="flex h-20 bg-alura-200 dark:bg-dark-200 justify-between items-center px-5 sm:rounded-xl sm:m-5">
-        <span className="text-gray-100">
-            Olá, Usuário
-        </span>
-        <h1 className="text-gray-100 text-xl">
-            Alura Newsletter
-        </h1>
-        <ToggleTheme />
+    <div className="p-5 w-full sm:m-5 sm:rounded-xl bg-alura-200 dark:bg-dark-200 flex justify-between items-center">
+      <span className="text-gray-100">Olá, { user || "Usuário" }</span>
+      <h1 className="text-gray-100">Alura Newsletter</h1>
+      <ToggleTheme />
     </div>
   )
 }
